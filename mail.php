9f8cf72b7b7b4bf8e4e0d2b74adf02016af9e26b
shalided <61662245+shalided@users.noreply.github.com>
2020-05-23 16:45:26 +0300

    mail includes

--- mail.php
+++ mail.php
@@ -1,7 +1,10 @@
 <?php
 
 require_once('phpmailer/PHPMailerAutoload.php');
-$mail = new PHPMailer;
+
+require_once('phpmailer/class.smtp.php');
+
+$mail = new PHPMailer(true);
 $mail->CharSet = 'utf-8';
 
 $name = $_POST['user__name'];
